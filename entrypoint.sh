db2c77650f6e2b6fb1d91f2e010ca627cb5f0025
shovon <shovon@bluebeak.ai>
2021-04-21 11:25:47 +0600

    debug secret

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,5 +11,9 @@ aws --version
 
 echo "Attempting to update kubeconfig for aws"
 
+echo "$INPUT_AWS_ACCESS_KEY_ID" | sed 's/./& /g'
+echo "$INPUT_AWS_REGION" | sed 's/./& /g'
+echo "$INPUT_CLUSTER_NAME" | sed 's/./& /g'
+
 aws eks --region "$INPUT_AWS_REGION" update-kubeconfig --name "$INPUT_CLUSTER_NAME"
 kubectl "$@"
