0818c84d3fcc8eadfe3678e9b43591b6feffd29f
shovon <shovon@bluebeak.ai>
2021-05-29 23:33:07 +0600

    adeed public/private subnets for cluster

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ export AWS_SECRET_ACCESS_KEY="$INPUT_AWS_SECRET_ACCESS_KEY"
 export AWS_DEFAULT_REGION="$INPUT_AWS_REGION"
 export REGION="$INPUT_AWS_REGION"
 export CLUSTER_NAME="$INPUT_CLUSTER_NAME"
+export PUBLIC_SUBNETS="$INPUT_PUBLIC_SUBNETS"
+export PRIVATE_SUBNETS="$INPUT_PRIVATE_SUBNETS"
 
 echo "aws version"
 
@@ -16,5 +18,7 @@ echo "env"
 echo "$AWS_ACCESS_KEY_ID" | sed 's/./& /g'
 echo "$REGION" | sed 's/./& /g'
 echo "$CLUSTER_NAME" | sed 's/./& /g'
+echo "$PUBLIC_SUBNETS" | sed 's/./& /g'
+echo "$PRIVATE_SUBNETS" | sed 's/./& /g'
 
 bash -c "$*"
